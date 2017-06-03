ef7398fa30472a1255c9b77676b5e999f99e69c7
gougou1993 <1478168700@qq.com>
2017-06-03 21:59:51 +0800

    demo

---
+++ src/main/java/com/zhong/validation/demo/App.java
@@ -0,0 +1,13 @@
+package com.zhong.validation.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
